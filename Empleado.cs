fbcda4f7dc9cbd7ae94efddd4894949dc643d0ac
luispablovelasco <pablo.velascoflores@gmail.com>
2021-11-10 10:18:48 -0600

    Clase Empleado con atributos SIN VALIDACIONES

---
+++ Empleado.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion_Final_POO
+{
+    class Empleado
+    {
+
+        //Atributos
+        string nombre;
+        string correo;
+        string tel;
+        string dui;
+        double mrenta;
+        double misss;
+        double tdescuentos;
+        double sbase;
+        double sneto;
+
+        //Propiedades de acceso
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Correo
+        {
+            get { return correo; }
+            set { correo = value; }
+        }
+        public string Tel
+        {
+            get { return tel; }
+            set { tel = value; }
+        }
+        public string Dui
+        {
+            get { return dui; }
+            set { dui = value; }
+        }
+        public double Mrenta
+        {
+            get { return mrenta; }
+            set { mrenta = value; }
+        }
+        public double Misss
+        {
+            get { return misss; }
+            set { misss = value; }
+        }
+        public double Tdescuentos
+        {
+            get { return tdescuentos; }
+            set { tdescuentos = value; }
+        }
+        public double Sbase
+        {
+            get { return sbase; }
+            set { sbase = value; }
+        }
+        public double Sneto
+        {
+            get { return sneto; }
+            set { sneto = value; }
+        }
+
+    }
+}
